d2277ad1db5784b3919763e0b91aa1b8262d414f
jonathanwlopes <jonathan@agenciaeplus.com.br>
2022-03-10 08:50:16 -0300

    important things

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,41 @@
-import React from "react"
-import { View, Text, StyleSheet } from "react-native"
+import React, { useState, useEffect } from "react"
+import { View, Text, StyleSheet, TextInput, FlatList } from "react-native"
+import { Button } from "../components/Button"
+import { SkillCard } from "../components/SkillCard"
 
 export function Home() {
+  const [newSkill, setNewSkill] = useState("")
+  const [mySkills, setMySkills] = useState([])
+  const [greeting, setGreeting] = useState("")
+
+  function handleAddNewSkill() {
+    setMySkills((oldState) => [...oldState, newSkill])
+  }
+
+  useEffect(() => {
+    const currentHour = new Date().getHours()
+
+    if (currentHour < 12) {
+      setGreeting("Good morning")
+    } else if (currentHour >= 12 && currentHour < 18) {
+      setGreeting("Good afternoon")
+    } else {
+      setGreeting("Good night")
+    }
+  }, [])
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome, Jonathan</Text>
+      <Text style={styles.greetings}>{greeting}</Text>
+
+      <TextInput style={styles.input} placeholder="New skill" placeholderTextColor="#555" onChangeText={setNewSkill} />
+
+      <Button onPress={handleAddNewSkill} />
+
+      <Text style={[styles.title, { marginVertical: 30 }]}>My Skills</Text>
+
+      <FlatList data={mySkills} keyExtractor={(item) => item} renderItem={({ item }) => <SkillCard skill={item} />} />
     </View>
   )
 }
@@ -13,9 +44,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#121015",
+    paddingHorizontal: 20,
+    paddingVertical: 70,
   },
 
   title: {
-    color: "#ffffff",
+    color: "#FFF",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+
+  input: {
+    backgroundColor: "#1f1e25",
+    color: "#FFF",
+    fontSize: 18,
+    padding: 10,
+    marginTop: 30,
+    borderRadius: 7,
+  },
+
+  greetings: {
+    color: "#FFF",
   },
 })
